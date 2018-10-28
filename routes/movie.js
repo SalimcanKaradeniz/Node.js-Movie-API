d45b242ed458b3a4597a899f2b7ab900a6a031b6
salimcan <salimcankaradeniz@gmail.com>
2018-10-28 15:57:27 +0300

    modelsimize created_date eklendi top10 ve beetwen end pointleri tamamlandı

--- routes/movie.js
+++ routes/movie.js
@@ -6,32 +6,43 @@ const router = express.Router();
 const Movie = require('../models/Movie');
 
 router.get('/', (req, res) => {
-   const promise = Movie.find({ });
-   promise.then((data) => {
-      res.json(data);
-   }).catch((err) => {
-       res.json(err);
-   });
+    const promise = Movie.find({});
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+// TOP 10
+
+router.get('/top10', (req, res) => {
+    const promise = Movie.find({}).limit(10).sort({imdb_score: -1});
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
 });
 
 router.get('/:movie_id', (req, res, next) => {
-   const promise = Movie.findById(req.params.movie_id);
-
-   promise.then((movie) => {
-       if (!movie)
-           next({ message: 'Aradığınız Movie Bulunamadı.', code: 404 });
-       res.json(movie);
-   }).catch((err) => {
-       res.json(err);
-   })
-});
-
-router.put('/:movie_id', (req, res, next) => {
-    const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new:true});
+    const promise = Movie.findById(req.params.movie_id);
 
     promise.then((movie) => {
         if (!movie)
-            next({ message: 'Aradığınız Movie Bulunamadı.', code: 404 });
+            next({message: 'Aradığınız Movie Bulunamadı.', code: 404});
+        res.json(movie);
+    }).catch((err) => {
+        res.json(err);
+    })
+});
+
+router.put('/:movie_id', (req, res, next) => {
+    const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new: true});
+
+    promise.then((movie) => {
+        if (!movie)
+            next({message: 'Aradığınız Movie Bulunamadı.', code: 404});
         res.json(movie);
     }).catch((err) => {
         res.json(err);
@@ -43,12 +54,13 @@ router.delete('/:movie_id', (req, res, next) => {
 
     promise.then((movie) => {
         if (!movie)
-            next({ message: 'Aradığınız Movie Bulunamadı.', code: 404 });
-        res.json({ status: 1 });
+            next({message: 'Aradığınız Movie Bulunamadı.', code: 404});
+        res.json({status: 1});
     }).catch((err) => {
         res.json(err);
     })
 });
+
 /* GET users listing. */
 router.post('/', (req, res, next) => {
     const {title, imdb_score, category, country, year} = req.body;
@@ -67,6 +79,21 @@ router.post('/', (req, res, next) => {
         res.json(data);
     });
 
+});
+
+// 2 yıl arasındaki Filmler
+router.get('/between/:start_year/:end_year', (req, res) => {
+    const {start_year, end_year} = req.params;
+    const promise = Movie.find(
+        {
+            year: {"$gte": parseInt(start_year), "$lte":parseInt(end_year)}
+        }
+    );
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
 });
 
 module.exports = router;
