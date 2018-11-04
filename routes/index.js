9944b955d8b7671da269d6ee0eb5212ced03cfa3
salimcan <salimcankaradeniz@gmail.com>
2018-11-04 14:27:27 +0300

    login/register end pointleri ve token işlemleri tamamlandı.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+// const app = require('app');
 
-/* GET home page. */
+const User = require('../models/Users');
+
+/* Anasayfa */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    res.render('index', {title: 'Express'});
+});
+
+router.post('/register', (req, res, next) => {
+    const { username, password} = req.body;
+
+    bcrypt.hash(password, 10).then((hash) => {
+        const user = new User({
+            username,
+            password : hash,
+        });
+
+        const promise = user.save();
+
+        promise.then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json(err);
+        });
+    });
+});
+
+router.post('/authenticate', (req, res, next) => {
+    const { username, password} = req.body;
+
+    User.findOne({
+        username,
+    }, (err, user) => {
+        if (err)
+            throw err;
+
+        if (!user){
+            res.json({
+                status: false,
+                message: 'Aradığınız kullanıcı bulunamadı'
+            });
+        }else{
+            bcrypt.compare(password, user.password).then((result) => {
+                if (!result){
+                    res.json({
+                        status: false,
+                        message: 'Kullanıcı adı ya da parolanız yanlış lütfen kontrol ediniz!'
+                    })
+                }else{
+                    const payload = {
+                        username
+                    };
+
+                    const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+                        expiresIn: 720 //12 saat eşittir
+                    });
+
+                    res.json({
+                        status: true,
+                        token
+                    })
+                }
+            });
+        }
+    });
+
 });
 
 module.exports = router;
